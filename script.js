c2522b6b315cc02ba8adf9fff0a83c39f5e03d94
dwoong93 <darylwoong@gmail.com>
2021-07-19 15:35:03 +0800

    Added leaflet map

---
+++ script.js
@@ -0,0 +1,19 @@
+//Coordinates for Singapore
+let singapore = [1.3558681517963378, 103.81259782407385];
+//Centre point on first load
+let map = L.map('map').setView(singapore, 12.48 ); 
+
+// setup the tile layers
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
+//add a button
+document.querySelector('darkmodebtn').addEventListener('click', function(){
+
+})
