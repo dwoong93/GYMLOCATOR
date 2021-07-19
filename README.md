c88663c63ed1c1d9d73f7424fbd337ffdcc7bbeb
dwoong93 <darylwoong@gmail.com>
2021-07-20 05:06:50 +0800

    added bus stop geoJSON

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GYMLOCATOR
+Bus stop geoJSON was taken from Lim Chee Aun (https://github.com/cheeaun), who scrapped and prepared the original data from https://www.lta.gov.sg/, Land Transport Authority.
--- script.js
+++ script.js
@@ -3,17 +3,40 @@ let singapore = [1.3558681517963378, 103.81259782407385];
 //Centre point on first load
 let map = L.map('map').setView(singapore, 12.48 ); 
 
-// setup the tile layers
+// Original map layer
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+    accessToken: 'pk.eyJ1IjoiZGFyeWx3b29uZyIsImEiOiJja3I5cDcwMWI0YjE1MnBxaG04ZjQ2MGE4In0.vQSEV1EzN_aerLwJlfmswA'
 }).addTo(map);
 
-//add a button
-document.querySelector('darkmodebtn').addEventListener('click', function(){
+//Cluster grouping for bus stops
+
+
+//Reading in bus stop location geojson file.
+window.addEventListener('DOMContentLoaded', async function(){
+    let response = await axios.get("data/busstops.geojson");
+    // let busStopsCluster = L.MarkerClusterGroup();
+    let busStopsLayer = L.geoJson(response.data, {
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("hello")}})
+    busStopsLayer.addTo(map);
+    busStopsCluster.addTo(map)
+
 
 })
+
+
+
+
+
+
+
+
+// //add a button
+// document.querySelector('darkmodebtn').addEventListener('click', function(){
+
+// })
