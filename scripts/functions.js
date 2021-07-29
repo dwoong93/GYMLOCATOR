41b8038e0542c9abc599693f64f87577d6d2a595
dwoong93 <darylwoong@gmail.com>
2021-07-30 06:05:12 +0800

    explored darkmode functionality using bootstrap button, set zoomoutlimit and set zoomsnap to 0.25 to allow proper fit of map

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,64 +1,7 @@
-//To allow zoom to region when Town is selected from dropdown list
 
-function zoomOnChange(selectedTown) {
-    if (selectedTown == "Pasir-Ris") {
-        map.setView(bedokCoor, 16);
-    }
-    if (selectedTown == "Tampines") {
-        map.setView(bishanCoor, 16);
-    }
-    if (selectedTown == "Simei") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-    if (selectedTown == "") {
-        map.setView(bukitGombakCoor, 16);
-    }
-}
+
+
+
 
 
 //To create a function to loop through geoJson file to check for 
--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@
 //Coordinates for Singapore
 let singapore = [1.3558681517963378, 103.81259782407385];
 //Centre point on first load
-let map = L.map('singapore-map',{maxBounds:[[1.4724179719905892, 104.00802298417915],[1.2113590252812299, 103.61150331088808]] 
-}, {zoomDelta: 0, zoomSnap: 0.25}
-).setView([1.3558681517963378, 103.81259782407385],13); 
+let map = L.map('singapore-map', {
+    zoomSnap: 0.25, minZoom: 12.25, maxBounds: [[1.4724179719905892, 104.00802298417915], [1.2113590252812299, 103.61150331088808]]
+}).setView([1.3558681517963378, 103.81259782407385], 12.25);
 
 // let southWest = L.latLng(1.56073, 104.11475);
 // let northEast = L.latLng(1.16, 103.502);
@@ -14,13 +14,13 @@ let map = L.map('singapore-map',{maxBounds:[[1.4724179719905892, 104.00802298417
 L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
     {
-    attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken:
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
             'pk.eyJ1IjoiZGFyeWx3b29uZyIsImEiOiJja3I5cDcwMWI0YjE1MnBxaG04ZjQ2MGE4In0.vQSEV1EzN_aerLwJlfmswA'
     }
 ).addTo(map);
@@ -32,10 +32,10 @@ L.tileLayer(
 let defaultMap = L.layerGroup();
 
 let OneMapSG_Default = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
-	minZoom: 11,
-	maxZoom: 18,
-	bounds: [[1.56073, 104.11475], [1.16, 103.502]],
-	attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
+    minZoom: 11,
+    maxZoom: 18,
+    bounds: [[1.56073, 104.11475], [1.16, 103.502]],
+    attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
 }).addTo(defaultMap)
 
 //One MapSG Night
@@ -43,30 +43,30 @@ let OneMapSG_Default = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x
 let darkModeMap = L.layerGroup();
 
 let OneMapSG_Night = L.tileLayer('https://maps-{s}.onemap.sg/v3/Night/{z}/{x}/{y}.png', {
-	minZoom: 11,
-	maxZoom: 18,
-	bounds: [[1.56073, 104.11475], [1.16, 103.502]],
-	attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
-}).addTo (darkModeMap)
+    minZoom: 11,
+    maxZoom: 18,
+    bounds: [[1.56073, 104.11475], [1.16, 103.502]],
+    attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
+}).addTo(darkModeMap)
 
 
 
 //icon for clubs
 const clubIcon = L.icon({
     iconUrl: 'images/clublogo.png',
-    iconSize: [40,40],
+    iconSize: [40, 40],
     iconAnchor: [20, 25]
 })
 
 
 //////////////////////////////////Importing of data//////////////////////////////////
-window.addEventListener('DOMContentLoaded', async function(){
+window.addEventListener('DOMContentLoaded', async function () {
 
-//////////////////////////////////Gym Clubs//////////////////////////////////////////
-//Layer group for Anytime Fitness Club - All
+    //////////////////////////////////Gym Clubs//////////////////////////////////////////
+    //Layer group for Anytime Fitness Club - All
     let allClubs = L.layerGroup();
 
-//Clustering for for Anytime Fitness Club Locations
+    //Clustering for for Anytime Fitness Club Locations
     let clubCluster = L.markerClusterGroup({
         spiderfyOnMaxZoom: false,
         showCoverageOnHover: false,
@@ -74,69 +74,78 @@ window.addEventListener('DOMContentLoaded', async function(){
     });
     clubCluster.addTo(allClubs)
 
-//Loading geoJson data for gym clubs
-        let response = await axios.get('data/CLUB.geojson');
-        let clubCoords = response.data.features;
-        let clubs = L.geoJson(response.data,{
-            onEachFeature: function(feature,layer){
-                layer.bindPopup(feature.properties.Club + feature.properties.Address)
-            }
-        }).addTo(clubCluster)
+    //Loading geoJson data for gym clubs
+    let response = await axios.get('data/CLUB.geojson');
+    let clubCoords = response.data.features;
+    let clubs = L.geoJson(response.data, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.Club + feature.properties.Address)
+        }
+    }).addTo(clubCluster)
 
-//getting outlet names and putting them into an array
-        let outletNames = [];
-        for (let x=0; x<clubCoords.length; x++){
-            outletNames.push (clubCoords[x].properties.Club)}
-        console.log (outletNames);   
+    //getting outlet names and putting them into an array
+    let outletNames = [];
+    for (let x = 0; x < clubCoords.length; x++) {
+        outletNames.push(clubCoords[x].properties.Club)
+    }
+    console.log(outletNames);
 
-//getting outlet coordinates and putting them into an array       
-        let outletCoords = [];
-        for (let i=0; i<clubCoords.length; i++){
-            outletCoords.push (clubCoords[i].geometry.coordinates)}
-        console.log (outletCoords);
+    //getting outlet coordinates and putting them into an array       
+    let outletCoords = [];
+    for (let i = 0; i < clubCoords.length; i++) {
+        outletCoords.push(clubCoords[i].geometry.coordinates)
+    }
+    console.log(outletCoords);
 
-// Combining outlet names and coordinates into key-value pairs.
-//when "sengkang" is clicked, a for loop will run to search for coordinates of the sites in buangkok, sengkang and punggol area through the Name Key.
-//These coordinates will then be stored into an array that is automated using a function, to highlight a polygon area. 
-        let keyValPair = outletCoords.reduce(function(keyValPair,field,index){
-            outletCoords[outletNames[index]] = field;
-            return keyValPair;
-        }, {})
-        // console.log(keyValPair);
+    // Combining outlet names and coordinates into key-value pairs.
+    //when "sengkang" is clicked, a for loop will run to search for coordinates of the sites in buangkok, sengkang and punggol area through the Name Key.
+    //These coordinates will then be stored into an array that is automated using a function, to highlight a polygon area. 
+    let keyValPair = outletCoords.reduce(function (keyValPair, field, index) {
+        outletCoords[outletNames[index]] = field;
+        return keyValPair;
+    }, {})
+    // console.log(keyValPair);
 
-        
-        
 
-////////////////////////BUS STOPS//////////////////////////////////////////
 
-//Layer group for bus stops - All
+
+    ////////////////////////BUS STOPS//////////////////////////////////////////
+
+    //Layer group for bus stops - All
     let allBusStops = L.layerGroup()
     //Loading geoJson data for bus stop loc and info
-        let response2 = await axios.get('data/BUS.geojson');
-        let busLayer = L.geoJson(response2.data,{
-            onEachFeature: function(feature,layer){
-                layer.bindPopup(feature.properties.busStopName)
-            }
-        }).addTo(allBusStops)
+    let response2 = await axios.get('data/BUS.geojson');
+    let busLayer = L.geoJson(response2.data, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.busStopName)
+        }
+    }).addTo(allBusStops)
 
 
-    
-//Layer control
-    let baseLayer ={
+
+    //Layer control
+    let baseLayer = {
         'OneMapSG': defaultMap,
         'Dark Mode': darkModeMap
-    } 
+    }
     let overlays = {
         // 'Dark Mode': darkModeMap
         'Show All Clubs': allClubs,
         'Show Bus Stops': allBusStops
-        
+
     }
 
     L.control.layers(baseLayer, overlays).addTo(map)
     allBusStops.addTo(map);
     allClubs.addTo(map);
-    
+
+
+
+
+    let DarkmodeMap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Night/{z}/{x}/{y}.png', {
+        minZoom: 11, maxZoom: 18, bounds: [[1.56073, 104.11475], [1.16, 103.502]], 
+        attribution: '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>'
+    })
 
     let houganglatlng = [1.372135151181164, 103.8859292830705]
     let punggollatlng = [1.401469018441145, 103.90283016626528]
@@ -144,78 +153,104 @@ window.addEventListener('DOMContentLoaded', async function(){
     let sengkanglatlng = [1.3886648098226932, 103.8939135551916]
     let buangkoklatlng = [1.3839157436675633, 103.87736690869015]
     let circle = null
+    let Darkmode = null
 
-    document.getElementById("ShowAll-btn").addEventListener("click", function(e){
+    document.getElementById("ShowAll-btn").addEventListener("click", function (e) {
         // e.preventDefault()
-        map.flyTo([1.3558681517963378, 103.81259782407385],12)
-        if (circle !=null){
+        map.flyTo([1.3558681517963378, 103.81259782407385], 12.75)
+        if (circle != null) {
             map.removeLayer(circle)
         }
-        
-        
+
+
 
     })
-    document.getElementById("Hougang-btn").addEventListener("click", function(e){
+    document.getElementById("Hougang-btn").addEventListener("click", function (e) {
         // e.preventDefault()
-        map.flyTo(houganglatlng,15)
-        if (circle !=null){
+        map.flyTo(houganglatlng, 15)
+        if (circle != null) {
             map.removeLayer(circle)
         }
-        circle = L.circle(houganglatlng, {radius: 1500})
+        circle = L.circle(houganglatlng, { radius: 1500 })
         map.addLayer(circle)
-        
+
 
     })
 
-    document.getElementById("Punggol-btn").addEventListener("click", function(e){
+    document.getElementById("Punggol-btn").addEventListener("click", function (e) {
         // e.preventDefault()
-        map.flyTo(punggollatlng,15)
-        if (circle !=null){
+        map.flyTo(punggollatlng, 15)
+        if (circle != null) {
             map.removeLayer(circle)
         }
-        circle = L.circle(punggollatlng, {radius: 1500})
+        circle = L.circle(punggollatlng, { radius: 1500 })
         map.addLayer(circle)
-        
+
 
     })
 
-    document.getElementById("Sengkang-btn").addEventListener("click", function(e){
+    document.getElementById("Sengkang-btn").addEventListener("click", function (e) {
         // e.preventDefault()
-        map.flyTo(sengkanglatlng,15)
-        if (circle !=null){
+        map.flyTo(sengkanglatlng, 15)
+        if (circle != null) {
             map.removeLayer(circle)
         }
-        circle = L.circle(sengkanglatlng, {radius: 1600})
+        circle = L.circle(sengkanglatlng, { radius: 1600 })
         map.addLayer(circle)
-        
+
 
     })
 
-    document.getElementById("Buangkok-btn").addEventListener("click", function(e){
+    document.getElementById("Buangkok-btn").addEventListener("click", function (e) {
         // e.preventDefault()
-        map.flyTo(buangkoklatlng,15)
-        if (circle !=null){
+        map.flyTo(buangkoklatlng, 15)
+        if (circle != null) {
             map.removeLayer(circle)
         }
-        circle = L.circle(buangkoklatlng, {radius: 1500})
+        circle = L.circle(buangkoklatlng, { radius: 1500 })
         map.addLayer(circle)
-        
+
 
     })
 
-    document.getElementById("Serangoon-btn").addEventListener("click", function(e){
+    document.getElementById("Serangoon-btn").addEventListener("click", function (e) {
         // e.preventDefault()
-        map.flyTo(serangoonlatlng,15)
-        if (circle !=null){
+        map.flyTo(serangoonlatlng, 15)
+        if (circle != null) {
             map.removeLayer(circle)
         }
-        circle = L.circle(serangoonlatlng, {radius: 1700})
+        circle = L.circle(serangoonlatlng, { radius: 1700, fillColor:"#7e3ff2", color: "yellow", fillOpacity:0.5})
         map.addLayer(circle)
-        
+
 
     })
+
+
+    document.getElementById("darkmodebtn").addEventListener("click", function (e) {
+        // e.preventDefault()
+        if (DarkmodeMap != null) {
+            OneMapSG_Default.addTo(defaultMap)
+        }
+        map.addLayer(DarkmodeMap).addTo(defaultMap)
+    })
+
+
     
 
-//end of async function
-})
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //end of async function
+    })
 
